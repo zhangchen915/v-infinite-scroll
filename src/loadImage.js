1ea06d217364ab11876be97dc8b6f6a7523c1e09
zhangchen <zhangchen915@outlook.com>
2019-07-18 02:30:08 +0800

    🔖: img load

--- src/loadImage.js
+++ src/loadImage.js
@@ -1,12 +1,24 @@
 const ctx = Symbol();
 
-const loadImage = {
+const loadImage = option => ({
     bind(el, binding, vnode) {
         el[ctx] = {
             vm: vnode.context,
             config: binding.value
         };
+
+        if (typeof binding.value === "string") el[ctx].config = {src: binding.value};
+        el[ctx].config = Object.assign(option, el[ctx].config);
+        console.log(el[ctx].config)
+
         const {
+            loading = '',
+            // TODO
+            backgroundConfig = {
+                size: 'cover',
+                position: 'center',
+                repeat: 'no-repeat'
+            },
             observerOption = {
                 root: null,
                 rootMargin: "0px",
@@ -14,28 +26,30 @@ const loadImage = {
             }
         } = el[ctx].config;
 
-        el[ctx].vm.$on('hook:updated', () => {
-            // if (!url) return;
-            const observer = new IntersectionObserver(([entry]) => {
-                if (entry && entry.isIntersecting) {
-                    //TODO set src
-                    observer.unobserve(el)
-                }
-            }, observerOption);
-            observer.observe(el);
-        });
+        if (el[ctx].config.loading) el.src = el[ctx].config.loading;
+    },
+    componentUpdated(el, binding, vnode) {
+        if (!el[ctx].config.src) return;
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry && entry.isIntersecting) {
+                console.log(el[ctx].config.src);
+                el.nodeName === "IMG" ? el.src = el[ctx].config.src : el.style.backgroundImage = `url(${el[ctx].config.src})`;
+                observer.unobserve(el);
+            }
+        }, el[ctx].config.observerOption);
+        observer.observe(el);
     },
     unbind(el) {
-
     }
-};
+});
 
-const loadImageContent = {}
+const loadImageContent = option => ({});
 
 export default {
-    install: (Vue) => {
+    install: (Vue, options = {}) => {
         document.styleSheets[0].insertRule('img:not([src]) {opacity: 0;}');
-        Vue.directive("img", loadImage);
+        if (options.loading) new Image().src = options.loading;
+        Vue.directive("img", loadImage(options));
         Vue.directive("img-content", loadImageContent)
     }
 }

f87fd5074f74bac981abb19209a557820a956a06
zhangchen <zhangchen915@outlook.com>
2019-07-21 22:53:34 +0800

    ✨: add webp support

--- src/loadImage.js
+++ src/loadImage.js
@@ -1,4 +1,13 @@
 const ctx = Symbol();
+const isWebp = Symbol('webp');
+
+function supportWebp() {
+    const elem = document.createElement('canvas');
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+    }
+    return false;
+}
 
 function assign(target, source) {
     if (typeof source !== "object") return source;
@@ -15,12 +24,18 @@ const loadImage = option => ({
             config: binding.value
         };
 
+        const {
+            loading = ''
+        } = el[ctx].config;
+
         if (typeof binding.value === "string") el[ctx].config = {src: binding.value};
         el[ctx].config = assign(el[ctx].config, option);
 
+        if (loading) el.src = loading;
+    },
+    inserted(el, binding, vnode) {
         const {
-            loading = '',
-            // TODO
+            webp,
             backgroundConfig = {
                 size: 'cover',
                 position: 'center',
@@ -33,16 +48,15 @@ const loadImage = option => ({
             }
         } = el[ctx].config;
 
-        if (el[ctx].config.loading) el.src = el[ctx].config.loading;
-    },
-    inserted(el, binding, vnode) {
         if (!el[ctx].config.src) return;
         const observer = new IntersectionObserver(([entry]) => {
+            let src = el[ctx].config.src;
+            src = window[isWebp] ? webp(src) : src;
             if (entry && entry.isIntersecting) {
-                el.nodeName === "IMG" ? el.src = el[ctx].config.src : el.style.backgroundImage = `url(${el[ctx].config.src})`;
+                el.nodeName === "IMG" ? el.src = src : el.style.backgroundImage = `url(${src})`;
                 observer.unobserve(el);
             }
-        }, el[ctx].config.observerOption);
+        }, observerOption);
         observer.observe(el);
     },
     unbind(el) {
@@ -54,7 +68,9 @@ const loadImageContent = option => ({});
 export default {
     install: (Vue, options = {}) => {
         document.styleSheets[0].insertRule('img:not([src]) {opacity: 0;}');
+        window[isWebp] = supportWebp();
         if (options.loading) new Image().src = options.loading;
+        if (typeof options.webp !== "function") throw new ReferenceError('webp must be function!');
         Vue.directive("img", loadImage(options));
         Vue.directive("img-content", loadImageContent)
     }

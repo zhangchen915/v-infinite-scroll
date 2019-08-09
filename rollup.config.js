c17ab6cb34fd71499e02c8e8d9241e3a5454e70f
zhangchen <zhangchen915@outlook.com>
2019-08-09 17:01:32 +0800

    update: build esm file

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,14 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: 'src/index.js',
-    output: {
-        name: 'lazy',
-        file: 'dist/lazy-load.js',
+    output: [{
+        file: 'dist/lazy-load.esm.js',
+        format: 'esm',
+    },{
+        name:'lazy',
+        file: 'dist/lazy-load.umd.js',
         format: 'umd',
-    },
+    }],
     plugins: [
         nodeResolve({
             jsnext: true
